db7e0da7b0b4a20396f5cc2e60f7a9aa374d4213
Roman Skrypchuk <roman.skrypchuk.pm.2019@lpnu.ua>
2021-10-24 23:18:38 +0300

    lab1 finish

--- lab1/task3.js
+++ lab1/task3.js
@@ -1,13 +1,12 @@
-module.exports.InsertionSort = function (array){
-    let key, i;
-    for(let k = 1; k < array.length; k++){
-      key = array[k];
-      i = k - 1;
-      while((i >= 0) && (array[i] > key)){
-        array[i + 1] = array[i];
-        i--;
-      }
-      array[i + 1] = key;
+module.exports.InsertionSort = function (array) {
+  let key, i
+  for (let k = 1; k < array.length; k++) {
+    key = array[k]
+    i = k - 1
+    while ((i >= 0) && (array[i] > key)) {
+      array[i + 1] = array[i]
+      i--
     }
+    array[i + 1] = key
   }
-  
+}
